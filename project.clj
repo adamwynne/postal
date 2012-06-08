21526a8839b0f81f75402051d4e8135ce15e9b52
Adam Wynne <adamwynne@gmail.com>
2012-06-08 10:40:57 +0100

    changed the project to go into my clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject com.draines/postal "1.8-SNAPSHOT"
+(defproject org.clojars.adamwynne/postal "1.8-SNAPSHOT"
   :repositories {"java.net" "http://download.java.net/maven/2"}
+  :plugins [[lein-clojars "0.7.0"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [javax.mail/mail "1.4.4"
                   :exclusions [javax.activation/activation]]])
